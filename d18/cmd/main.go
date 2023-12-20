35061c661e0121099117403baed9c541e95c2a18
Spike Curtis <spike@coder.com>
2023-12-20 09:04:44 +0400

    Use shoelace + Pick's theorem

--- d18/cmd/main.go
+++ d18/cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	dp := d18.ParseDigPlan(d18.GetInput("./input"), 1)
-	fmt.Println(dp.CountInterior2())
+	fmt.Println(dp.CountInterior())
 	dp = d18.ParseDigPlan(d18.GetInput("./input"), 2)
-	fmt.Println(dp.CountInterior2())
+	fmt.Println(dp.CountInterior())
 }
--- d18/pkg.go
+++ d18/pkg.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -56,197 +55,38 @@ const (
 
 func (dp DigPlan) CountInterior() int {
 	cur := Point{0, 0}
-	dug := map[Point]bool{cur: true}
-	prevD := dp.Trenches[len(dp.Trenches)-1].D
-	cirality := 0
+	prev := cur
+
+	border := 0
+	shoelace := 0
 	for _, t := range dp.Trenches {
-		c := t.D - prevD
-		if c < -1 {
-			c += 4
-		}
-		if c > 1 {
-			c -= 4
-		}
-		if c < -1 || c > 1 {
-			panic("cirality assumption violated")
-		}
-		cirality += int(c)
-		prevD = t.D
-		for i := 0; i < t.Len; i++ {
-			cur = cur.Move(t.D, 1)
-			dug[cur] = true
-		}
-	}
-	if cur.x != 0 || cur.y != 0 {
-		panic("didn't reach origin")
-	}
-	if cirality > 0 {
-		cirality = 1
-	} else {
-		cirality = -1
-	}
-
-	// add adjacent interior points to open set
-	open := make(map[Point]bool)
-	for _, t := range dp.Trenches {
-		for i := 0; i < t.Len; i++ {
-			if i > 1 && i < t.Len-1 {
-				d := (t.D + Direction(cirality) + 4) % 4
-				open[cur.Move(d, 1)] = true
-			}
-			cur = cur.Move(t.D, 1)
-		}
-	}
-
-	// fill
-	pop := func() Point {
-		for p := range open {
-			delete(open, p)
-			return p
-		}
-		panic("empty")
-	}
-	for len(open) > 0 {
-		f := pop()
-		dug[f] = true
-		for _, d := range []Direction{Up, Down, Left, Right} {
-			fp := f.Move(d, 1)
-			if !dug[fp] {
-				open[fp] = true
-			}
-		}
-	}
-	return len(dug)
-}
-
-func (dp DigPlan) CountInterior2() int {
-	cur := Point{0, 0}
-	xm := make(map[int]bool)
-	ym := make(map[int]bool)
-
-	segments := make([]Segment, 0)
-	for _, t := range dp.Trenches {
-		xm[cur.x] = true
-		ym[cur.y] = true
-		x0 := cur.x
-		y0 := cur.y
 		cur = cur.Move(t.D, t.Len)
-		segments = append(segments, Segment{
-			Trench: t,
-			Xmin:   min(cur.x, x0),
-			Xmax:   max(cur.x, x0),
-			Ymin:   min(cur.y, y0),
-			Ymax:   max(cur.y, y0),
-		})
+		border += t.Len
+		// "shoelace" formula computes the area from point coordinates
+		// can be derived from Green's theorem
+		shoelace += cur.y * prev.x
+		shoelace -= cur.x * prev.y
+		prev = cur
 	}
-	xs := make([]int, 0, len(xm))
-	for x := range xm {
-		xs = append(xs, x)
+	if shoelace < 0 {
+		shoelace = -shoelace
 	}
-	sort.Ints(xs)
-	xs = append(xs, xs[len(xs)-1])
-	ys := make([]int, 0, len(ym))
-	for y := range ym {
-		ys = append(ys, y)
-	}
-	sort.Ints(ys)
-	ys = append(ys, ys[len(ys)-1])
-
-	interior := 0
-	for i := 0; i < len(xs)-1; i++ {
-		x0 := xs[i]
-		x1 := xs[i+1]
-
-		for j := 0; j < len(ys)-1; j++ {
-			y0 := ys[j]
-			y1 := ys[j+1]
-			sq := Square{x0, x1, y0, y1}
-			interior += sq.CountInterior(segments)
-		}
-	}
-	return interior
-}
-
-type Square struct {
-	x0, x1 int
-	y0, y1 int
-}
-
-// CountInterior counts the number of interior tiles in a Square.
-//
-// We only count the top-left corner, top, left and middle of the square, leaving
-// off the bottom-left, bottom-right and top-right corners, the bottom, and the right
-// this allows us to avoid double counting, since those tiles are in other squares in the array
-//
-// *--------*-----*--
-// |        |     |
-// *--------*-----*--
-// |        |     |
-func (s Square) CountInterior(segs []Segment) int {
-	interior := 0
-	mx := (s.x1-s.x0)/2 + s.x0
-	my := (s.y1-s.y0)/2 + s.y0
-	if IsInterior(Point{s.x0, s.y0}, segs) {
-		interior++
-	}
-	// Left
-	if my != s.y0 && IsInterior(Point{s.x0, my}, segs) {
-		interior += s.y1 - s.y0 - 1
-	}
-	// Top
-	if mx != s.x0 && IsInterior(Point{mx, s.y0}, segs) {
-		interior += s.x1 - s.x0 - 1
-	}
-	// Middle
-	if mx != s.x0 && my != s.y0 && IsInterior(Point{mx, my}, segs) {
-		interior += (s.x1 - s.x0 - 1) * (s.y1 - s.y0 - 1)
-	}
-	return interior
+	shoelace = shoelace / 2
+	// shoelace gives the area of the polygon defined by the center of the trenches,
+	// but we want to count border + interior points, since we dig 1m x1m cubes
+	// at all these points.
+	// Pick's theorem: A = i + b/2 - 1 where
+	// A = area (calculated by shoelace formula above)
+	// b = border points
+	// i = interior points
+	//
+	// Some algebra gives: i + b = A + b/2 + 1
+	return shoelace + (border / 2) + 1
 }
 
 type Trench struct {
 	D   Direction
 	Len int
-}
-
-type Segment struct {
-	Trench
-	Xmin, Xmax int
-	Ymin, Ymax int
-}
-
-func IsInterior(p Point, segs []Segment) bool {
-	wn := 0
-	for _, seg := range segs {
-		if seg.Contains(p) {
-			return true
-		}
-		wn += WindingNumberInc(p, seg)
-	}
-	return wn != 0
-}
-
-func (s Segment) Contains(p Point) bool {
-	return p.x >= s.Xmin && p.x <= s.Xmax && p.y >= s.Ymin && p.y <= s.Ymax
-}
-
-func WindingNumberInc(p Point, s Segment) int {
-	if s.D != Up && s.D != Down {
-		return 0
-	}
-	if p.x >= s.Xmin {
-		return 0
-	}
-	if p.y <= s.Ymin {
-		return 0
-	}
-	if p.y > s.Ymax {
-		return 0
-	}
-	if s.D == Up {
-		return 1
-	}
-	return -1
 }
 
 func ParseDigPlan(lines []string, part int) DigPlan {
